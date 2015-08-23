4b7a5cd3bc0571cc22ab40b6b6d8b48452e06ff0
Steve Wake <stephen.wake@careers.govt.nz>
2015-08-23 18:46:33 +1200

    fix change of region code javascript to update order form

--- javascript/RegionalShippingModifierField.js
+++ javascript/RegionalShippingModifierField.js
@@ -1,5 +1,5 @@
 ;(function($) {
-	$('.order-form').on('change', 'select.region-code,.modifier-set-field select', function(e) {
+	$('.order-form').on('change', 'select.region-code', function (e) {
 		$('.order-form').entwine('sws').updateCart();
 	});
 })(jQuery);
